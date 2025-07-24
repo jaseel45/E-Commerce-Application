8f655847007b79677e62886effe5d2b54a7d32d2
jaseel45 <muhammedjaseel40@gmail.com>
2025-07-24 23:17:34 +0530

    fix: cors error

--- Backend/server.js
+++ Backend/server.js
@@ -12,10 +12,7 @@ dotenv.config();
 const app = express();
 
 // Middlewares
-app.use(cors({
-  origin: 'http://localhost:5173',
-  credentials: true
-}));
+app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 
